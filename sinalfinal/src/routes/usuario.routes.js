5213886b8624d5df8389c4398dc06ef5f5de6f3e
Jose_Jasson <jbzdev777@gmail.com>
2025-06-09 21:04:37 -0300

    usuario.routes.js

--- sinalfinal/src/routes/usuario.routes.js
+++ sinalfinal/src/routes/usuario.routes.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const UsuarioController = require("../controllers/UsuarioController");
+const UsuarioController = require('../controllers/UsuarioController');
 
-router.post("/", UsuarioController.create);
-router.get("/", UsuarioController.list);
-router.get("/:id", UsuarioController.show);
-router.put("/:id", UsuarioController.update);
-router.delete("/:id", UsuarioController.remove);
+router.post('/', UsuarioController.create);
+router.get('/', UsuarioController.list);
+router.get('/:id', UsuarioController.show);
+router.put('/:id', UsuarioController.update);
+router.delete('/:id', UsuarioController.remove);
 
 module.exports = router;
