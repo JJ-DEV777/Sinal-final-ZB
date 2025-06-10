aaaa0a6169c481ae047b3a1cbf00351574e18556
Jose_Jasson <jbzdev777@gmail.com>
2025-06-09 21:03:16 -0300

    backup.routes.js

--- sinalfinal/src/routes/backup.routes.js
+++ sinalfinal/src/routes/backup.routes.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const UsuarioController = require("../controllers/UsuarioController");
+const BackupController = require('../controllers/BackupController');
 
-router.post("/", UsuarioController.create);
-router.get("/", UsuarioController.list);
-router.get("/:id", UsuarioController.show);
-router.put("/:id", UsuarioController.update);
-router.delete("/:id", UsuarioController.remove);
+router.post('/', BackupController.create);
+router.get('/', BackupController.list);
+router.get('/:id', BackupController.show);
+router.put('/:id', BackupController.update);
+router.delete('/:id', BackupController.remove);
 
 module.exports = router;
