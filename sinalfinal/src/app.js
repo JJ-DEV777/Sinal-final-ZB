cbb732b9593b6587d7360d3bf8bc90178f8e77e1
Jose_Jasson <jbzdev777@gmail.com>
2025-06-09 21:25:02 -0300

    Update app.js

--- sinalfinal/src/app.js
+++ sinalfinal/src/app.js
@@ -1,8 +1,22 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const routes = require("./src/routes");
+const { port } = require("./src/config/env");
+
 const app = express();
-const routes = require("./routes");
 
 app.use(express.json());
+
 app.use("/api", routes);
+
+app.use(cors({
+  origin: '*', // Permite apenas este frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
 
 module.exports = app;
